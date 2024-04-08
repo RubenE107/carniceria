2cf2750440a02af19648e0dfccc7577432c5f62d
RubenE107 <irtu098@gmail.com>
2024-04-08 14:04:55 -0600

    CORRECION PARA PRODUCION

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URI: "http://localhost:3000/api",
-  API_URI_CORREOS: "http://localhost:3001"
+    API_URI_CORREO: "http://localhost:3001",
   };
 /*
  * For easier debugging in development mode, you can import the following file

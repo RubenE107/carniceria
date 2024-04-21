a2ae012939791b89ec560e035d5b15f1438a70c8
JorgeAEV <127627359+JorgeAEV@users.noreply.github.com>
2024-04-21 17:58:01 -0600

    Produccion Corregido

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URI: "https://dtapi.axlserial.me/api/",
-  API_URI_CORREO: "https://dtmail.axlserial.me/",
+  API_URI_CORREO: "https://dtmail.axlserial.me",
   API_URI_IMAGENES: "https://dtimagenes.axlserial.me"
 };

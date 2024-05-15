5f73c275c981b4c40b1b820a11447688959dd403
Kevin Caballero <Gamemode948a@gmail.com>
2024-05-14 23:05:08 -0600

    arreglo modelo venta

--- src/app/models/Venta.ts
+++ src/app/models/Venta.ts
@@ -1,6 +1,5 @@
 export class Venta{
     id: number;
-    id_usuario: number
     id_producto: number;
     monto: number;
     fecha: string;
@@ -9,7 +8,6 @@ export class Venta{
     name:string;
     constructor() {
         this.id = 0;
-        this.id_usuario= 0;
         this.id_producto= 0;
         this.monto= 0;
         this.fecha = '';
